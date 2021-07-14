afb1b73bd21a18f5b573d3ff7c2063baa18b42ea
ceptoplex <ceptoplex@gmail.com>
2021-07-14 21:59:44 +0200

    Add logging

--- src/app.py
+++ src/app.py
@@ -1,3 +1,6 @@
+import logging
+import sys
+
 import discord
 
 from config import Config
@@ -5,6 +8,15 @@ from config import Config
 
 class App:
     def __init__(self):
+        logger = logging.getLogger()
+        logger.setLevel(logging.INFO)
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_formatter = logging.Formatter(
+            fmt='[%(asctime)s] [%(levelname)s] [%(name)s] %(message)s',
+            datefmt='%Y-%m-%dT%H:%M:%S%z')
+        console_handler.setFormatter(console_formatter)
+        logger.addHandler(console_handler)
+
         self.client = discord.Client()
 
     def run(self):
